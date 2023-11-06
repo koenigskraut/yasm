143c8945dec27d0d41a8f949c5b55f781b2d6fb4
koenigskraut <koenigskraut1@gmail.com>
2023-11-06 16:40:39 +0300

    add yasm executable

--- build.zig
+++ build.zig
@@ -5,6 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const libyasm_stdint = b.addWriteFile("libyasm-stdint.h", libyasm_stdint_data);
+    const license_c = b.addWriteFile("license.c", license_data);
 
     const libyasm = b.addStaticLibrary(.{
         .name = "yasm",
@@ -13,7 +14,7 @@ pub fn build(b: *std.Build) !void {
         .link_libc = true,
     });
     libyasm.addIncludePath(.{ .path = "." });
-    libyasm.addCSourceFiles(.{ .files = libyasm_srsc });
+    libyasm.addCSourceFiles(.{ .files = libyasm_srsc, .flags = &.{"-fno-sanitize=undefined"} });
     libyasm.defineCMacro("YASM_LIB_SOURCE", null);
     libyasm.defineCMacro("HAVE_CONFIG_H", null);
     libyasm.addIncludePath(libyasm_stdint.getDirectory());
@@ -36,12 +37,38 @@ pub fn build(b: *std.Build) !void {
         .HAVE__STRICMP = null,
         // Define to 1 if you have the `toascii' function.
         .HAVE_TOASCII = 1,
-        //Name of package
+        // Name of package
         .PACKAGE = "yasm",
+        // Define to the address where bug reports for this package should be sent.
+        .PACKAGE_BUGREPORT = "bug-yasm@tortall.net",
+        // Define to the full name of this package.
+        .PACKAGE_NAME = "yasm",
+        // Define to the full name and version of this package.
+        .PACKAGE_STRING = "@PACKAGE_STRING@",
+
+        // // Define to the version of this package.
+        // .PACKAGE_VERSION = "@PACKAGE_VERSION@",
+        // .VERSION = PACKAGE_VERSION,
     });
     libyasm.addConfigHeader(config_h);
     inline for (libyasm_headers) |h| libyasm.installHeader(h, h);
-    b.installArtifact(libyasm);
+    // b.installArtifact(libyasm);
+
+    const yasm_exe = b.addExecutable(.{
+        .name = "yasm",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    yasm_exe.addIncludePath(.{ .path = "." });
+    yasm_exe.addIncludePath(.{ .path = "frontends/yasm" });
+    yasm_exe.addIncludePath(libyasm_stdint.getDirectory());
+    yasm_exe.addIncludePath(license_c.getDirectory());
+    yasm_exe.addCSourceFiles(.{ .files = &.{ "frontends/yasm/yasm.c", "frontends/yasm/yasm-options.c" }, .flags = &.{"-fno-sanitize=undefined"} });
+    yasm_exe.defineCMacro("HAVE_CONFIG_H", null);
+    yasm_exe.addConfigHeader(config_h);
+    yasm_exe.linkLibrary(libyasm);
+    b.installArtifact(yasm_exe);
 }
 
 const StrSlice = []const []const u8;
@@ -139,4 +166,70 @@ const libyasm_stdint_data =
     \\ // #endif
     \\
     \\ #endif
+;
+
+const license_data =
+    \\static const char *license_msg[] = {
+    \\    "Yasm is Copyright (c) 2001-2014 Peter Johnson and other Yasm developers.",
+    \\    "",
+    \\    "Yasm developers and/or contributors include:",
+    \\    "  Peter Johnson",
+    \\    "  Michael Urman",
+    \\    "  Brian Gladman (Visual Studio build files, other fixes)",
+    \\    "  Stanislav Karchebny (options parser)",
+    \\    "  Mathieu Monnier (SSE4 instruction patches, NASM preprocessor additions)",
+    \\    "  Anonymous \"NASM64\" developer (NASM preprocessor fixes)",
+    \\    "  Stephen Polkowski (x86 instruction patches)",
+    \\    "  Henryk Richter (Mach-O object format)",
+    \\    "  Ben Skeggs (patches, bug reports)",
+    \\    "  Alexei Svitkine (GAS preprocessor)",
+    \\    "  Samuel Thibault (TASM parser and frontend)",
+    \\    "",
+    \\    "-----------------------------------",
+    \\    "Yasm licensing overview and summary",
+    \\    "-----------------------------------",
+    \\    "",
+    \\    "Note: This document does not provide legal advice nor is it the actual",
+    \\    "license of any part of Yasm.  See the individual licenses for complete",
+    \\    "details.  Consult a lawyer for legal advice.",
+    \\    "",
+    \\    "The primary license of Yasm is the 2-clause BSD license.  Please use this",
+    \\    "license if you plan on submitting code to the project.",
+    \\    "",
+    \\    "Yasm has absolutely no warranty; not even for merchantibility or fitness",
+    \\    "for a particular purpose.",
+    \\    "",
+    \\    "-------",
+    \\    "Libyasm",
+    \\    "-------",
+    \\    "Libyasm is 2-clause or 3-clause BSD licensed, with the exception of",
+    \\    "bitvect, which is triple-licensed under the Artistic license, GPL, and",
+    \\    "LGPL.  Libyasm is thus GPL and LGPL compatible.  In addition, this also",
+    \\    "means that libyasm is free for binary-only distribution as long as the",
+    \\    "terms of the 3-clause BSD license and Artistic license (as it applies to",
+    \\    "bitvect) are fulfilled.",
+    \\    "",
+    \\    "-------",
+    \\    "Modules",
+    \\    "-------",
+    \\    "The modules are 2-clause or 3-clause BSD licensed.",
+    \\    "",
+    \\    "---------",
+    \\    "Frontends",
+    \\    "---------",
+    \\    "The frontends are 2-clause BSD licensed.",
+    \\    "",
+    \\    "-------------",
+    \\    "License Texts",
+    \\    "-------------",
+    \\    "The full text of all licenses are provided in separate files in the source",
+    \\    "distribution.  Each source file may include the entire license (in the case",
+    \\    "of the BSD and Artistic licenses), or may reference the GPL or LGPL license",
+    \\    "file.",
+    \\    "",
+    \\    "BSD.txt - 2-clause and 3-clause BSD licenses",
+    \\    "Artistic.txt - Artistic license",
+    \\    "GNU_GPL-2.0 - GNU General Public License",
+    \\    "GNU_LGPL-2.0 - GNU Library General Public License",
+    \\};
 ;
