5bc1e7f82e3a18bdc80c65613c56f27cd8cab46d
koenigskraut <koenigskraut1@gmail.com>
2023-11-07 03:42:42 +0300

    hardcode everything

--- build.zig
+++ build.zig
@@ -21,22 +21,73 @@ pub fn build(b: *std.Build) !void {
     const config_h = b.addConfigHeader(.{ .include_path = "config.h" }, .{
         // Define if shared libs are being built
         .BUILD_SHARED_LIBS = 0,
+        // Define to 1 if you have the GNU C Library
+        .HAVE_GNU_C_LIBRARY = null,
         // Define if messsage translations are enabled
         .ENABLE_NLS = 1,
         // Define if you have the `abort' function.
         .HAVE_ABORT = 1,
+        .HAVE_CATGETS = null,
+        // Define to 1 if you have the Mac OS X function
+        // CFLocaleCopyPreferredLanguages in the CoreFoundation framework.
+        .HAVE_CFLOCALECOPYPREFERREDLANGUAGES = null,
+        // Define to 1 if you have the Mac OS X function CFPreferencesCopyAppValue in
+        // the CoreFoundation framework.
+        .HAVE_CFPREFERENCESCOPYAPPVALUE = null,
         // Define to 1 if you have the <libgen.h> header file.
         .HAVE_LIBGEN_H = 1,
-        // Define to 1 if you have the <unistd.h> header file.
-        .HAVE_UNISTD_H = 1,
         // Define to 1 if you have the <direct.h> header file.
         .HAVE_DIRECT_H = null,
+        // Define if the GNU dcgettext() function is already present or preinstalled.
+        .HAVE_DCGETTEXT = 1,
+        // Define to 1 if you have the `ftruncate' function.
+        .HAVE_FTRUNCATE = 1,
         // Define to 1 if you have the `getcwd' function.
         .HAVE_GETCWD = 1,
-        // Define to 1 if you have the `_stricmp' function.
-        .HAVE__STRICMP = null,
+        .HAVE_GETTEXT = 1,
+        // Define if you have the iconv() function and it works.
+        .HAVE_ICONV = null,
+        // Define to 1 if you have the <inttypes.h> header file.
+        .HAVE_INTTYPES_H = 1,
+        .HAVE_LC_MESSAGES = null,
+        // Define to 1 if you have the `mergesort' function.
+        .HAVE_MERGESORT = null,
+        // Define to 1 if you have the `popen' function.
+        .HAVE_POPEN = 1,
+        // Define to 1 if you have the <stdint.h> header file.
+        .HAVE_STDINT_H = 1,
+        // Define to 1 if you have the <stdio.h> header file.
+        .HAVE_STDIO_H = 1,
+        // Define to 1 if you have the <stdlib.h> header file.
+        .HAVE_STDLIB_H = 1,
+        .HAVE_STPCPY = null,
+        // Define to 1 if you have the `strcasecmp' function.
+        .HAVE_STRCASECMP = 1,
+        // Define to 1 if you have the `strcmpi' function.
+        .HAVE_STRCMPI = null,
+        // Define to 1 if you have the `stricmp' function.
+        .HAVE_STRICMP = null,
+        // Define to 1 if you have the <strings.h> header file.
+        .HAVE_STRINGS_H = 1,
+        // Define to 1 if you have the <string.h> header file.
+        .HAVE_STRING_H = 1,
+        // Define to 1 if you have the `strncasecmp' function.
+        .HAVE_STRNCASECMP = 1,
+        // Define to 1 if you have the `strsep' function.
+        .HAVE_STRSEP = 1,
+        // Define to 1 if you have the <sys/stat.h> header file.
+        .HAVE_SYS_STAT_H = 1,
+        // Define to 1 if you have the <sys/types.h> header file.
+        .HAVE_SYS_TYPES_H = 1,
         // Define to 1 if you have the `toascii' function.
         .HAVE_TOASCII = 1,
+        // Define to 1 if you have the <unistd.h> header file.
+        .HAVE_UNISTD_H = 1,
+        // Define to 1 if you have the `vsnprintf' function.
+        .HAVE_VSNPRINTF = 1,
+        // Define to 1 if you have the `_stricmp' function.
+        .HAVE__STRICMP = null,
+
         // Name of package
         .PACKAGE = "yasm",
         // Define to the address where bug reports for this package should be sent.
@@ -44,11 +95,41 @@ pub fn build(b: *std.Build) !void {
         // Define to the full name of this package.
         .PACKAGE_NAME = "yasm",
         // Define to the full name and version of this package.
-        .PACKAGE_STRING = "@PACKAGE_STRING@",
+        .PACKAGE_STRING = "yasm 1.3.0",
+        // Define to the one symbol short name of this package.
+        .PACKAGE_TARNAME = "yasm",
+        // Define to the home page for this package.
+        .PACKAGE_URL = "",
+        // Define to the version of this package.
+        .PACKAGE_VERSION = "1.3.0",
+        // Define to 1 if the C compiler supports function prototypes.
+        .PROTOTYPES = 1,
 
-        // // Define to the version of this package.
-        // .PACKAGE_VERSION = "@PACKAGE_VERSION@",
-        // .VERSION = PACKAGE_VERSION,
+        // Command name to run C preprocessor
+        .CPP_PROG = "zig cc -E",
+
+        // The size of `char', as computed by sizeof.
+        .SIZEOF_CHAR = null,
+        // The size of `int', as computed by sizeof.
+        .SIZEOF_INT = null,
+        // The size of `long', as computed by sizeof.
+        .SIZEOF_LONG = null,
+        // The size of `short', as computed by sizeof.
+        .SIZEOF_SHORT = null,
+        // The size of `void*', as computed by sizeof.
+        .SIZEOF_VOIDP = null,
+        // Define to 1 if all of the C90 standard headers exist (not just the ones
+        // required in a freestanding environment). This macro is provided for
+        // backward compatibility; new code need not use it.
+        .STDC_HEADERS = 1,
+        // Version number of package
+        .VERSION = "1.3.0",
+        // Define if using the dmalloc debugging malloc package
+        .WITH_DMALLOC = null,
+        // Define like PROTOTYPES; this can be used by system headers.
+        .__PROTOTYPES = 1,
+        // Define to empty if `const' does not conform to ANSI C.
+        .@"const" = null,
     });
     libyasm.addConfigHeader(config_h);
     inline for (libyasm_headers) |h| libyasm.installHeader(h, h);
@@ -74,6 +155,47 @@ pub fn build(b: *std.Build) !void {
 const StrSlice = []const []const u8;
 
 const libyasm_srsc: StrSlice = &.{
+    "modules/arch/x86/x86arch.c",
+    "modules/arch/x86/x86bc.c",
+    "modules/arch/x86/x86expr.c",
+    "modules/arch/x86/x86id.c",
+    "modules/arch/lc3b/lc3barch.c",
+    "modules/arch/lc3b/lc3bbc.c",
+    "modules/listfmts/nasm/nasm-listfmt.c",
+    "modules/parsers/gas/gas-parser.c",
+    "modules/parsers/gas/gas-parse.c",
+    "modules/parsers/gas/gas-parse-intel.c",
+    "modules/parsers/nasm/nasm-parser.c",
+    "modules/parsers/nasm/nasm-parse.c",
+    "modules/preprocs/nasm/nasm-preproc.c",
+    "modules/preprocs/nasm/nasm-pp.c",
+    "modules/preprocs/nasm/nasmlib.c",
+    "modules/preprocs/nasm/nasm-eval.c",
+    "modules/preprocs/raw/raw-preproc.c",
+    "modules/preprocs/cpp/cpp-preproc.c",
+    "modules/preprocs/gas/gas-preproc.c",
+    "modules/preprocs/gas/gas-eval.c",
+    "modules/dbgfmts/codeview/cv-dbgfmt.c",
+    "modules/dbgfmts/codeview/cv-symline.c",
+    "modules/dbgfmts/codeview/cv-type.c",
+    "modules/dbgfmts/dwarf2/dwarf2-dbgfmt.c",
+    "modules/dbgfmts/dwarf2/dwarf2-line.c",
+    "modules/dbgfmts/dwarf2/dwarf2-aranges.c",
+    "modules/dbgfmts/dwarf2/dwarf2-info.c",
+    "modules/dbgfmts/null/null-dbgfmt.c",
+    "modules/dbgfmts/stabs/stabs-dbgfmt.c",
+    "modules/objfmts/dbg/dbg-objfmt.c",
+    "modules/objfmts/bin/bin-objfmt.c",
+    "modules/objfmts/elf/elf.c",
+    "modules/objfmts/elf/elf-objfmt.c",
+    "modules/objfmts/elf/elf-x86-x86.c",
+    "modules/objfmts/elf/elf-x86-amd64.c",
+    "modules/objfmts/elf/elf-x86-x32.c",
+    "modules/objfmts/coff/coff-objfmt.c",
+    "modules/objfmts/coff/win64-except.c",
+    "modules/objfmts/macho/macho-objfmt.c",
+    "modules/objfmts/rdf/rdf-objfmt.c",
+    "modules/objfmts/xdf/xdf-objfmt.c",
     "libyasm/assocdat.c",
     "libyasm/bitvect.c",
     "libyasm/bc-align.c",
@@ -82,7 +204,7 @@ const libyasm_srsc: StrSlice = &.{
     "libyasm/bc-org.c",
     "libyasm/bc-reserve.c",
     "libyasm/bytecode.c",
-    "libyasm/cmake-module.c",
+    // "libyasm/cmake-module.c",
     "libyasm/errwarn.c",
     "libyasm/expr.c",
     "libyasm/file.c",
@@ -103,6 +225,12 @@ const libyasm_srsc: StrSlice = &.{
     "libyasm/value.c",
     "libyasm/xmalloc.c",
     "libyasm/xstrdup.c",
+    "x86cpu.c",
+    "x86regtmod.c",
+    "lc3bid.c",
+    "gas-token.c",
+    "nasm-token.c",
+    "module.c",
 };
 
 const libyasm_headers: StrSlice = &.{
